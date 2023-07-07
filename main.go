d4fead42b3ef3b966651af266aef1af250e9cdd3
jubeiam <jubeiam@gmail.com>
2023-07-07 20:03:15 +0200

    initial

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
+)
+
+func main() {
+
+	app := fiber.New(fiber.Config{
+		Views: html.New("./views", ".html"),
+	})
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("index", fiber.Map{
+			"Title": "Hello, World!",
+		})
+	})
+
+	log.Fatal(app.Listen(":3000"))
+}
