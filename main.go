161b1d3735ed919027448322d17c5fb80813b0da
jubeiam <jubeiam@gmail.com>
2023-07-07 20:52:18 +0200

    feat: replace name with other template

--- main.go
+++ main.go
@@ -5,17 +5,25 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html/v2"
+	"github.com/qinains/fastergoding"
 )
 
 func main() {
+	fastergoding.Run() // hot reload
 
 	app := fiber.New(fiber.Config{
 		Views: html.New("./views", ".html"),
 	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.Render("index", fiber.Map{
-			"Title": "Hello, World!",
+		return c.Render("pages/index", fiber.Map{
+			"Title": "Hello, Iwona!",
+		}, "layouts/main")
+	})
+
+	app.Post("/random-name", func(c *fiber.Ctx) error {
+		return c.Render("partials/name", fiber.Map{
+			"Title": "Hello, Leszek!",
 		})
 	})
 
